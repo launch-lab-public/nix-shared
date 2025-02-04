4b692b91f0b5f81444a30d09ee8af038f03880b2
Lauri Niskanen <ape@ape3000.com>
2025-02-04 13:47:18 +0200

    Add unstable packages with a workaround overlay
    The overlay can be removed when bitwarden-cli issues have been fixed.

--- flake.nix
+++ flake.nix
@@ -2,16 +2,28 @@
   description = "Shared Nix definitions for a development environment";
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+    nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs =
     {
       self,
       nixpkgs,
+      nixpkgs-unstable,
       flake-utils,
       ...
     }:
     let
+      # Needed for bitwarden-cli: https://github.com/NixOS/nixpkgs/issues/339576
+      bitwardenOverlay = final: prev: {
+        bitwarden-cli = prev.bitwarden-cli.overrideAttrs (oldAttrs: {
+          nativeBuildInputs = (oldAttrs.nativeBuildInputs or [ ]) ++ [
+            prev.llvmPackages_18.stdenv.cc
+          ];
+          stdenv = prev.llvmPackages_18.stdenv;
+        });
+      };
+
       project =
         pkgsConfig: f:
         flake-utils.lib.eachDefaultSystem (
@@ -21,8 +33,17 @@
               inherit system;
               config = pkgsConfig;
             };
+
+            unstablePkgs = import nixpkgs-unstable {
+              inherit system;
+              overlays = [ bitwardenOverlay ];
+            };
+
+            pkgsWithUnstable = pkgs // {
+              unstable = unstablePkgs;
+            };
           in
-          (f pkgs)
+          (f pkgsWithUnstable)
           // {
             formatter = pkgs.nixfmt-rfc-style;
           }
